a442c142c0067c8a8147a854bae03e5d697f6c76
Sidhesh Parab <42218395+Everlasting12@users.noreply.github.com>
2025-12-01 23:22:01 +0530

    improve pattern 12

--- patterns/pattern-12.js
+++ patterns/pattern-12.js
@@ -1,28 +1,26 @@
 
 /**
-1
-23
-456
-78910
-1112131415
 
- */
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+
+*/
 
 
 function printPattern(n){
   let count = 1
   for(let i = 1; i <=n; i++){
     let row = ''
-    
-    
+
     // numbers
     for(let j = 1; j <= i; j++){
-      row+=count
+      row+=count + ' '
       count++
     }
-    
     console.log(row)
-   
   }
 }
 printPattern(5)
