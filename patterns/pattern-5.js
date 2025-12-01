8a60e518639f7b1cd4ee288e587d767b946586e7
Sidhesh Parab <42218395+Everlasting12@users.noreply.github.com>
2025-12-01 16:13:26 +0530

    pattern5 added

---
+++ patterns/pattern-5.js
@@ -0,0 +1,22 @@
+/**
+
+*****
+****
+***
+**
+*
+
+
+*/
+
+function printPattern(n){
+  
+  for(let i = 0; i < n; i++){
+    let row = ''
+    for(let j = n; j > i; j--){
+      row+= '*'
+    }
+    console.log(row)
+  }
+}
+printPattern(5)
