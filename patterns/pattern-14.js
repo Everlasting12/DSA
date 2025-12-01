dd7af0e9f881314a791ea32323877246c9dd6d68
Sidhesh Parab <42218395+Everlasting12@users.noreply.github.com>
2025-12-01 23:36:16 +0530

    pattern 14 added

---
+++ patterns/pattern-14.js
@@ -0,0 +1,23 @@
+/**
+
+ABCDE
+ABCD
+ABC
+AB
+A
+
+*/
+
+
+function printPattern(n){
+  for(let i = 1; i <=n; i++){
+
+    let row = ''
+    // alphabets
+    for(let j = 1; j <= n - i + 1; j++){
+      row+= String.fromCharCode(64 + j)
+    }
+    console.log(row)
+  }
+}
+printPattern(5)
