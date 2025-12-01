ac5b931442d253aa6781e64ff6515f189b318d60
Sidhesh Parab <42218395+Everlasting12@users.noreply.github.com>
2025-12-01 16:07:16 +0530

    pattern2 added

---
+++ patterns/pattern-2.js
@@ -0,0 +1,21 @@
+/**
+
+*
+**
+***
+****
+*****
+
+*/
+
+function printPattern(n){
+  
+  for(let i = 0; i < n; i++){
+    let row = ''
+    for(let j = 0; j <= i; j++){
+      row+= '*'
+    }
+    console.log(row)
+  }
+}
+printPattern(5)
