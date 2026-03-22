e387762be067a224e17d36d3f411b478d2d509c6
sidheshparab <sidheshparab34@gmail.com>
2026-03-22 23:39:19 +0530

    program to find the maximum profit cab be booked

---
+++ numbers/best-time-to-buy-sell-stock.js
@@ -0,0 +1,20 @@
+function maxProfit(prices) {
+  let maxProfit = 0;
+
+  let left = 0,
+    right = 1;
+
+  while (right < prices.length) {
+    if (prices[right] > prices[left]) {
+      const profit = prices[right] - prices[left];
+      maxProfit = Math.max(maxProfit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+
+  return maxProfit;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
