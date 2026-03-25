13c6a659a5460046d89869b318633075d8133173
sidheshparab <sidheshparab34@gmail.com>
2026-03-25 23:27:14 +0530

    program to find if the number is a power of two using recursion

--- recursion/power-of-two.js
+++ recursion/power-of-two.js
@@ -1,6 +1,6 @@
 function isPowerOfTwo(n) {
   if (n === 1) return true;
-  else if (n < 0 || n % 2 !== 0) return false;
+  else if (n < 1 || n % 2 !== 0) return false;
 
   return isPowerOfTwo(n / 2);
 }
