af8f6c13b2895311c8b0b0cbc7e0fedb57d8ff50
sidheshparab <sidheshparab34@gmail.com>
2026-03-26 00:19:57 +0530

    program to find nth fibonacci number and sum of all n fibonacci numbers using recursion

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -14,3 +14,21 @@ console.log("fibonacci(2)", fibonacci(2));
 console.log("fibonacci(3)", fibonacci(3));
 console.log("fibonacci(4)", fibonacci(4));
 console.log("fibonacci(8)", fibonacci(8));
+
+function fib(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let prev2 = 0; // F(0)
+  let prev1 = 1; // F(1)
+
+  for (let i = 2; i <= n; i++) {
+    let current = prev1 + prev2;
+    prev2 = prev1;
+    prev1 = current;
+  }
+
+  return prev1;
+}
+
+console.log("fiv(8)", fibonacci(8)); // 21
