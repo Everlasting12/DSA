5c031b4f7b8c7b7bcdbc6bc581cc4da033338dbc
sidheshparab <sidheshparab34@gmail.com>
2026-03-27 23:15:53 +0530

    alternative way of swapping an element in the bubble sort algo

--- searching-sorting/bubble-sort.js
+++ searching-sorting/bubble-sort.js
@@ -3,9 +3,12 @@ function bubbleSort(arr) {
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        let swap = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = swap;
+        // let swap = arr[j];
+        // arr[j] = arr[j + 1];
+        // arr[j + 1] = swap;
+
+        // -------- or -----------
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
