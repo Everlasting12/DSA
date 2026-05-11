1adabadd03778b1ac8248c21757ff78a639ea947
Sidhesh Parab <42218395+Everlasting12@users.noreply.github.com>
2026-05-11 15:56:10 +0530

    Add searchInsert function for array position search
    Implement searchInsert function to find target index or position.

---
+++ arrays/search-insert-position.mjs
@@ -0,0 +1,26 @@
+function searchInsert(nums, target) {
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === target) return i;
+
+        if(nums[i] > target) {
+            return i;
+        }
+    }
+
+    return nums.length;
+};
+
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
