a09667d713d7a21f36fe40e42c81264bebf95aa3
Sidhesh Parab <42218395+Everlasting12@users.noreply.github.com>
2026-09-11 22:23:48 +0530

    Create balance-binary-tree.mjs

---
+++ trees/binary-tree/balance-binary-tree.mjs
@@ -0,0 +1,21 @@
+function balanceBinaryTree(root){
+    let ans = true;
+    
+    const calcHeight = (curr) => {
+       if(!curr){
+            return 0;
+        }
+        
+        let leftHeight = calcHeight(curr.left);
+        let rightHeight = calcHeight(curr.right);
+        
+        if(Math.abs(leftHeight-rightHeight) > 1) {
+            ans = ans && false;
+        }
+        
+        return 1+Math.max(leftHeight,rightHeight)
+    }
+    calcHeight(root);
+    
+    return ans;
+}
