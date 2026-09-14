caf62fb7c5ba1de4eca3b3960467884735ec6b32
Sidhesh Parab <42218395+Everlasting12@users.noreply.github.com>
2026-09-14 23:22:55 +0530

    Create subtree-of-binary-tree.mjs

---
+++ trees/binary-tree/subtree-of-binary-tree.mjs
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    let rootHash = serialize(root);
+    let subRootHash = serialize(subRoot);
+    
+    return rootHash.includes(subRootHash);
+};
+
+function serialize(root){
+    let hash = "";
+    
+    function traverse(curr){
+        if(!curr){
+            hash = hash + "-#";
+            return 
+        }
+        hash = hash + "-" + curr.val;
+        traverse(curr.left);
+        traverse(curr.right);
+    }
+    traverse(root);
+    
+    return hash;
+}
